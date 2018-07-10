93c70ade3b8b4dfe9334be91404798aa9ac708ae
Henry Doan <henrydoan96@gmail.com>
2018-07-10 12:20:22 -0600

    update and start filter

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,13 +4,13 @@ import Todo from './Todo';
 class TodoList extends Component {
 
   render() {
-    const { todos } = this.props;
+    const { todos, todoClick } = this.props;
 
     return (
       <div>
         { todos.map( t => {
           return (
-            <Todo key={t.id} title={t.title} />
+            <Todo key={t.id} id={t.id} title={t.title} complete={t.complete} todoClick={todoClick} />
           )
         }) }
       </div>
